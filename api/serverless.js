2ac3dded8918a7512def86fad46a1bac7bc4696a
Luciano Bargmann <luciano.bargmann@gmail.com>
2023-06-15 17:15:44 -0300

    DNS Query complete

--- api/serverless.js
+++ api/serverless.js
@@ -15,42 +15,48 @@ const TOKEN = process.env.SSC_API_TOKEN;
 
 const ssc = SSC({ token: TOKEN, host: 'https://platform-api.securityscorecard.tech' });
 
-function readTxtRecords(domain) {
-
-  return dns.resolveTxt(domain, (err, records) => {
-    if (err) {
-      console.error('Error retrieving DNS TXT records:', err);
-      return;
-    }
-  
-    console.log('DNS TXT records for', domain);
-    records.forEach((record, index) => {
-      console.log(`Record ${index + 1}:`, record.join(', '));
+async function readTxtRecords(domain) {
+  return new Promise((resolve, reject) => {
+    dns.resolveTxt(domain, (err, records) => {
+      if (err) {
+        console.error('Error retrieving DNS TXT records:', err);
+        reject(err);
+      } else {
+        var out = "";
+        console.log('DNS TXT records for', domain);
+        records.forEach((record, index) => {
+          out += `Record ${index + 1}: ` + record.join(', ');
+          console.log(`Record ${index + 1}:`, record.join(', '))
+        });
+        resolve(out);
+      }
     });
-
-    return records;
   });
 }
 
-
 server.get('/', async (request, reply) => {
-  let txtRecords = readTxtRecords(process.env.TARGET_DOMAIN)
-  reply.send(txtRecords)
+  try {
+    let txtRecords = await readTxtRecords(process.env.TARGET_DOMAIN)
+    reply.send(txtRecords)
+  } catch (err) {
+    reply.code(500).send({ error: 'Could not fetch TXT records' });
+  }
 });
 
 // this is an event that could be dispatched by a custom event, 
 // for the sole purpose of the example, it will be propagated using a post event
 server.post('/signal', async(request, reply) => {
   try {
+    let txtRecords = await readTxtRecords(process.env.TARGET_DOMAIN);
     const signalResponse = await ssc.apps.sendSignals('signal_app.sample_information', [
-      { domain: 'example.com', summary: 'test signal using sdk' }]);
+      { domain: process.env.TARGET_DOMAIN, summary: 'TXT Records found: ' + txtRecords, }]);
     reply.send(signalResponse);
   } catch (error) {
     reply.send(error);
   }
 });
 
-server.get('/more-info', async (request, reply) => reply.send("detailed signal info"));
+server.get('/more-info', async (request, reply) => reply.send("Lists all TXT records in a domain"));
 
 server.register(static, {
   root: path.join(__dirname, '..', 'public'),
